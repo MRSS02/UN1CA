d852e12d0bb0ad940973d0cadedfd207ab35d637
ssch71 <s01093450498@gmail.com>
2026-07-28 22:09:23 +0900

    customize.sh 업데이트

--- target/dm1q/patches/kernel/customize.sh
+++ target/dm1q/patches/kernel/customize.sh
@@ -1,6 +1,5 @@
 LOG_STEP_IN "- Processing SM8550-Common Kernel by @GoRhanHee"
 
-PDR="$(pwd)"
 BOOT_IMG_URL="https://github.com/GoRhanHee/android_kernel_samsung_sm8550_common/releases/download/5.15.207/boot.img"
 KERNELSU_MANAGER_APK="https://github.com/GoRhanHee/android_kernel_samsung_sm8550_common/releases/download/5.15.206/KernelSU_Next_v3.2.0-21-g5a4a7187_33150-release.apk"
 
@@ -25,7 +24,7 @@ ADD_MANAGER_APK_TO_PRELOAD()
     sed -i "/system\/preload/d" "$WORK_DIR/configs/fs_config-system" \
         && sed -i "/system\/preload/d" "$WORK_DIR/configs/file_context-system"
     while read -r i; do
-        FILE="$(echo -n "$i"| sed "s.$WORK_DIR/system/..")"
+        FILE="${i/$WORK_DIR\/system\//}"
         [ -d "$i" ] && echo "$FILE 0 0 755 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
         [ -f "$i" ] && echo "$FILE 0 0 644 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
         FILE="$(echo -n "$FILE" | sed 's/\./\\./g')"
@@ -34,7 +33,7 @@ ADD_MANAGER_APK_TO_PRELOAD()
 
     rm -f "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"
     while read -r i; do
-        FILE="$(echo "$i" | sed "s.$WORK_DIR/system..")"
+        FILE="${i/$WORK_DIR\/system/}"
         echo "$FILE" >> "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"
     done <<< "$(find "$WORK_DIR/system/system/preload" -name "*.apk" | sort)"
 }
@@ -42,4 +41,4 @@ ADD_MANAGER_APK_TO_PRELOAD()
 REPLACE_KERNEL_BINARIES
 ADD_MANAGER_APK_TO_PRELOAD
 
-unset PDR KERNEL_URL BOOT_EDITOR_URL KERNELSU_MANAGER_APK
+unset BOOT_IMG_URL KERNELSU_MANAGER_APK
