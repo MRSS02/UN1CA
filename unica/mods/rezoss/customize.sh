4d256b537bfb2a1cc57a970cb29c144c00e5b832
MRSS02 <68190118+MRSS02@users.noreply.github.com>
2026-08-24 22:08:24 -0300

    Update customize.sh

--- unica/mods/rezoss/customize.sh
+++ unica/mods/rezoss/customize.sh
@@ -3,7 +3,7 @@ LOG_STEP_IN "- Rezoss experimental mods"
 # =============================================================================
 # Base Overlay
 # =============================================================================
-ADD_TO_WORK_DIR "$MODPATH" "product" "/product/overlay/framework-res_dm1qxxx_auto_generated_rro_product.apk" 0 0 644 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "$MODPATH" "product" "product/overlay/framework-res_dm1qxxx_auto_generated_rro_product.apk" 0 0 644 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "$MODPATH" "system" "." 0 0 755 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "$MODPATH" "system_ext" "." 0 0 755 "u:object_r:system_file:s0"
 LOG "- Adding optional AOSP zip boot animation support"
