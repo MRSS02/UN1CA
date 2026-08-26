d4ce9acb21183b746710405a837d4baa81754000
MRSS02 <68190118+MRSS02@users.noreply.github.com>
2026-08-26 11:17:04 -0300

    Update debloat.sh

--- unica/debloat.sh
+++ unica/debloat.sh
@@ -5,3 +5,7 @@
 # - Add entries inside the specific partition containing that file (<PARTITION>_DEBLOAT+="")
 # - DO NOT add the partition name at the start of any entry (eg. "/system/dpolicy_system")
 # - DO NOT add a slash at the start of any entry (eg. "/dpolicy_system")
+
+ADD_TO_WORK_DIR "m3qxxx" "system" \
+    "system/etc/permissions/signature-permissions-com.samsung.android.game.gamehome.xml" \
+    0 0 644 "u:object_r:system_file:s0"
