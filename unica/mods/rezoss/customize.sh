b4869cb72769b7c02b571b39e4c5bc86864684a9
MRSS02 <mateusyasd@hotmail.com>
2026-09-06 18:51:25 -0300

    added fullscreen apod overlay back

--- unica/mods/rezoss/customize.sh
+++ unica/mods/rezoss/customize.sh
@@ -6,6 +6,7 @@ LOG_STEP_IN "- Rezoss experimental mods"
 # =============================================================================
 # Base Overlay
 # =============================================================================
+ADD_TO_WORK_DIR "$MODPATH" "product" "overlay/framework-res__dm1qxxx__auto_generated_rro_product.apk" 0 0 644 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "$MODPATH" "system" "." 0 0 755 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "$MODPATH" "system_ext" "." 0 0 755 "u:object_r:system_file:s0"
 LOG "- Adding mountless AOSP zip boot animation support"
