ad3ff43aeb4d4154c945c16190367a526b36ea75
ssch71 <s01093450498@gmail.com>
2026-09-08 08:28:15 +0900

    customize.sh 업데이트

--- platform/sm8250/patches/camera/customize.sh
+++ platform/sm8250/patches/camera/customize.sh
@@ -84,7 +84,11 @@ do
 done
 
 # Pro mode EV Fix
-LOG_STEP_IN "- Adding camera PRO mode EV-fix"
-ADD_TO_WORK_DIR "r9qxxx" "vendor" \
-    "lib64/X12QS_libTsAe.so" 0 0 644 "u:object_r:vendor_file:s0"
-LOG_STEP_OUT
+if [ -f "prebuilts/samsung/r9qxxx/vendor/lib64/X12QS_libTsAe.so" ]; then
+    LOG_STEP_IN "- Adding camera PRO mode EV-fix"
+    ADD_TO_WORK_DIR "r9qxxx" "vendor" \
+        "lib64/X12QS_libTsAe.so" 0 0 644 "u:object_r:vendor_file:s0"
+    LOG_STEP_OUT
+else
+    echo "WARN: X12QS_libTsAe.so not found in r9qxxx, skipping EV-fix"
+fi
