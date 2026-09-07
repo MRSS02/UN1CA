d31f55e8e44054dac094c97969c40ec81da074d7
ssch71 <s01093450498@gmail.com>
2026-09-08 07:14:43 +0900

    customize.sh 업데이트

--- platform/sm8250/patches/camera/customize.sh
+++ platform/sm8250/patches/camera/customize.sh
@@ -83,12 +83,6 @@ do
     ADD_TO_WORK_DIR "p3qxxx" "system" "$blob" 0 0 644 "u:object_r:system_lib_file:s0" &
 done
 
-# HDR fix
-LOG_STEP_IN "- Adding hdr-fix"
-ADD_TO_WORK_DIR "r9qxxx" "system" \
-    "lib/libstagefright.so" 0 0 644 "u:object_r:system_file:s0"
-LOG_STEP_OUT
-
 # Pro mode EV Fix
 LOG_STEP_IN "- Adding camera PRO mode EV-fix"
 ADD_TO_WORK_DIR "r9qxxx" "vendor" \
